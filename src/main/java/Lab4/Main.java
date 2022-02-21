1d3e88ff6b88cab5703d312760013615d2485022
soblab <soblab@Whit216-17.it.farmingdale.edu>
2022-02-21 10:02:00 -0500

    Initial Commit - Created 1 Doc

---
+++ src/main/java/Lab4/Main.java
@@ -0,0 +1,30 @@
+package Lab4;
+
+import org.dizitart.no2.Document;
+import static org.dizitart.no2.Document.createDocument;
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.NitriteCollection;
+
+/**
+ *
+ * @author Suleman Ali
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //    System.out.println("Hello World");
+
+        //Create or Open the Database
+        Nitrite db = Nitrite.builder().filePath("./company.db").openOrCreate();
+
+        NitriteCollection collection = db.getCollection("employees");
+        Document doc = createDocument("id", 1)
+                .put("name" , "Rose Diaz ")
+                .put("salary", 100000);
+        // insert a document into the collection
+        collection.insert(doc);
+
+        //Close the database when you are done
+        db.close();
+    }
+}
